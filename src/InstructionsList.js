20b8a10e099d858fd12de231cef9dad178a65bce
JeffAllison <itsjallison@gmail.com>
2022-05-03 15:43:26 -0700

    added instructionlist.js dta

--- src/InstructionsList.js
+++ src/InstructionsList.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export default function InstructionsList() {
-  return <div>InstructionsList</div>;
+export default function InstructionsList({ instructions }) {
+  return (
+    <section>
+      <h3>Order Instructions:</h3>
+      {instructions.map((instruction, i) => (
+        <p key={instruction + i}>{instruction}</p>
+      ))}
+    </section>
+  );
 }
