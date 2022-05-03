53d826c2272bd988d19da9b4510900bbd6ab9f6d
JeffAllison <itsjallison@gmail.com>
2022-05-03 14:43:51 -0700

    imported .js files into app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import './App.css';
+import { useState } from 'react';
+import DrinkDropdown from './DrinkDropdown';
+import FoodDropdown from './FoodDropdown';
+import SideDropdown from './SideDropdown';
+import InstructionsList from './InstructionsList';
+import InstructionsForm from './InstructionsForm';
+import OrderNameInput from './OrderNameInput';
+import Order from './Order';
 
 function App() {
   return <div className="App"></div>;
