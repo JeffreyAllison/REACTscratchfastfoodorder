5d9862da0b0560767d09dc811c977d94b7656617
JeffAllison <itsjallison@gmail.com>
2022-05-03 15:22:01 -0700

    added fooddropdown.js data

--- src/FoodDropdown.js
+++ src/FoodDropdown.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export default function FoodDropdown() {
-  return <div>FoodDropdown</div>;
+export default function FoodDropdown({ setFood }) {
+  return (
+    <div>
+      Choose a Drink Option:
+      <select onChange={(e) => setFood(e.target.value)}>
+        <option value="food-1">Beef Wellington</option>
+        <option value="food-2">Kabobs</option>
+        <option value="food-3">Pad Thai</option>
+      </select>
+    </div>
+  );
 }
