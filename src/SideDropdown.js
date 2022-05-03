4fb90aa74027839b7f89826279485c4f4dd54c48
JeffAllison <itsjallison@gmail.com>
2022-05-03 15:18:48 -0700

    added sidedropdown.js data

--- src/SideDropdown.js
+++ src/SideDropdown.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export default function SideDropdown() {
-  return <div>SideDropdown</div>;
+export default function SideDropdown({ setSide }) {
+  return (
+    <div>
+      Choose a Drink Option:
+      <select onChange={(e) => setSide(e.target.value)}>
+        <option value="side-1">Dal</option>
+        <option value="side-2">Dosa</option>
+        <option value="side-3">Fries</option>
+      </select>
+    </div>
+  );
 }
