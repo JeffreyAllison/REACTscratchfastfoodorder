30ce785d566cabd59f79f7d54ffef646eff1bf26
JeffAllison <itsjallison@gmail.com>
2022-05-03 16:02:11 -0700

    added instructionformlist.js data

--- src/InstructionsForm.js
+++ src/InstructionsForm.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function InstructionsForm() {
-  return <div>InstructionsForm</div>;
+export default function InstructionsForm({ instructions, setInstructions }) {
+  const [instructionInForm, setInstructionInForm] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setInstructions([...instructions, instructionInForm]);
+  }
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        Add additional instructions to your order:
+        <input onChange={(e) => setInstructionInForm(e.target.value)} />
+        <button>Add Order Instructions</button>
+      </form>
+    </section>
+  );
 }
